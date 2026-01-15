a7035a0fff417441d3214fe278342e28b2e243fb
Newton1727 <narsing_s@ce.iitr.ac.in>
2026-01-16 01:44:09 +0530

    Update API base URL to production backend

--- client/api/axios.js
+++ client/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://bentoveda-early-access.onrender.com/",
 });
 
 export default api;
